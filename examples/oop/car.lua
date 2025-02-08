15b23c8ae14af0c6a10095a2a0b307c27809736f
cosinewaves <158834632+cosinewaves@users.noreply.github.com>
2025-02-08 20:46:50 +0000

    Update car.lua

--- examples/oop/car.lua
+++ examples/oop/car.lua
@@ -2,6 +2,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local proxie = require(ReplicatedStorage.proxie)
 
+-- Since the car type references this table twice, it's easier to store it as a single type.
 type CarProperties = {
 	Fuel: number,
 	DistanceTravelled: number,
@@ -12,7 +13,7 @@ type CarProperties = {
 type Car = {
 	Owner: string,
 	Properties: CarProperties,
-	_proxyTable: proxie.Proxy<CarProperties>,
+	_proxyTable: proxie.Proxy<CarProperties>, -- Ensure to pass CarProperties as a generic type to receive intellisense support
 }
 
 local function CarConstructorFunction(Owner: string): Car
@@ -27,10 +28,10 @@ local function CarConstructorFunction(Owner: string): Car
 	local self: Car = {
 		Owner = Owner,
 		Properties = properties,
-		_proxyTable = {} :: proxie.Proxy<CarProperties>
+		_proxyTable = {} :: proxie.Proxy<CarProperties> -- Typecasting the type stops the type solver having a tantrum in strict mode
 	}
 	
-	-- In strict mode, the type-solver will error this code. I am yet to make a fix so please create a PR if you know how to.
+	-- NOTE: In strict mode, the type-solver will error this code. I am yet to make a fix so please create a PR if you know how to.
 	self._proxyTable = proxie.createProxy(self.Properties, function(key: string, newValue: number | boolean, oldValue: number | boolean)
 		print(
 			"[" .. self.Owner .. "'s Car]: " .. key .. " has changed from " .. tostring(oldValue) .. " to " .. tostring(newValue) .. "."
